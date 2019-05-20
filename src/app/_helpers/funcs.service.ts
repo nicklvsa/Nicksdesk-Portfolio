abf5b920bd1f3f75b41d66db74400727a780da25
Nick Gerancher <nick@nicksdesk.com>
2019-05-20 10:27:41 -0400

    still broken - navigator might not inherit correct functions

--- src/app/_helpers/funcs.service.ts
+++ src/app/_helpers/funcs.service.ts
@@ -12,16 +12,15 @@ export class FuncsService {
 	}
 
 	//fix mobile check
-	isNotMobile() {
-		let nav: Navigator = new Navigator();
-		return !(
-			nav.userAgent.match(/Android/i)
- 			&& nav.userAgent.match(/webOS/i)
- 			&& nav.userAgent.match(/iPhone/i)
- 			&& nav.userAgent.match(/iPad/i)
- 			&& nav.userAgent.match(/iPod/i)
- 			&& nav.userAgent.match(/BlackBerry/i)
- 			&& nav.userAgent.match(/Windows Phone/i)
+	isNotMobile(): boolean {
+		return (
+			!navigator.userAgent.match(/Android/i)
+ 			&& !navigator.userAgent.match(/webOS/i)
+ 			&& !navigator.userAgent.match(/iPhone/i)
+ 			&& !navigator.userAgent.match(/iPad/i)
+ 			&& !navigator.userAgent.match(/iPod/i)
+ 			&& !navigator.userAgent.match(/BlackBerry/i)
+ 			&& !navigator.userAgent.match(/Windows Phone/i)
  		);
 	}
 }
