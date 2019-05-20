680a8d82209cff056c6bb35e8462284933a21ce6
Nick Gerancher <nick@nicksdesk.com>
2019-05-20 09:36:23 -0400

    added profile picture but it is bad

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,14 +11,25 @@ import { FuncsService } from '../_helpers/funcs.service';
 })
 export class DashboardComponent implements OnInit {
 
+	user_str: string = "";
+	user_img: string = "";
+
 	constructor(private authService: AuthService, private router: Router, public funcs: FuncsService) {
+		let raw: any = JSON.parse(localStorage.getItem("user"));
 		if(!authService.isLoggedIn) {
 			funcs.delay(2500).then(any => {
 				router.navigate(['/login']);
 			});
 		}
+		if(raw != null) {
+			this.user_str = (raw.email == null || raw.email == "") ? raw.displayName : raw.email;
+			this.user_img = (raw.photoURL == null || raw.photoURL == "") ? "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" : raw.photoURL;
+		} else {
+			this.user_str = "";
+			this.user_img = "";
+		}
+		
 	}
 
 	ngOnInit() {}
-
 }
