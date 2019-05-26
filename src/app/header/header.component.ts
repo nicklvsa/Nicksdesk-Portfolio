aeb02ff5ae882ad0acc2881e59f8deecc0b47983
nicksdesk <nick@nicksdesk.com>
2019-05-26 16:32:34 -0400

    added cool nav color cycler

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,10 +10,50 @@ import { FuncsService } from '../_helpers/funcs.service';
 export class HeaderComponent implements OnInit {
 
   isNavbarCollapsed: boolean = true;
+  navColorCode: string = "#007bff";
 
   constructor(private authService: AuthService, private funcs: FuncsService) {
-    //is_not_mobile not returning correct value
-    console.log(funcs.isMobile());
+    console.log("Mobile: " + funcs.isMobile());
+  }
+
+  cycleNavColor(comp: number) {
+    if(comp != null && comp > -1) {
+      switch(comp) {
+        case 0:
+          this.navColorCode = "#007bff";
+        break;
+        case 1:
+          this.navColorCode = "#0408B4";
+        break;
+        case 2:
+          this.navColorCode = "#00038B";
+        break;
+        case 3:
+          this.navColorCode = "#000378";
+        break;
+        case 4:
+          this.navColorCode = "#000368";
+        break;
+        case 5:
+          this.navColorCode = "#000250";
+        break;
+        case 6:
+          this.navColorCode = "#01023D";
+        break;
+        case 7:
+          this.navColorCode = "#000135";
+        break;
+        default:
+        this.navColorCode = "#007bff";
+        break;
+      }
+    }
+  }
+
+  get currNavBgStyler(): any {
+    return {
+      'background-color': (this.navColorCode != null) ? this.navColorCode : "#007bff"
+    };
   }
 
   ngOnInit() {}
