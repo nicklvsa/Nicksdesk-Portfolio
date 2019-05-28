e8a7016a83b2698bbd8f8788edd3b71dd825e391
nicksdesk <nick@nicksdesk.com>
2019-05-28 19:34:39 -0400

    adding ngfor directive to manage block route

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+interface BlogEntries {
+	id: number;
+	title: string;
+	content: string;
+}
+
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.scss']
 })
+
 export class BlogComponent implements OnInit {
 
-  constructor() { }
+	private entries: BlogEntries[];
 
-  ngOnInit() {
-  }
+	constructor() {
+		//fetch entries
+		this.entries = [
+			{id: 1, title: "test", content: "some content"},
+			{id: 2, title: "two", content: "second content"}
+		];
+	}
+
+	ngOnInit() {}
 
 }
